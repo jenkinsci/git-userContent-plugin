18cb8cccc987a28f5bf3e96853d2e7c7146ac5a6
Daniel Beck <daniel-beck@users.noreply.github.com>
2024-01-25 23:05:54 +0100

    Modernize, implement HTTP auth

--- src/main/java/org/jenkinsci/plugins/gitUserContent/GitUserContentRepository.java
+++ src/main/java/org/jenkinsci/plugins/gitUserContent/GitUserContentRepository.java
@@ -1,13 +1,19 @@
 package org.jenkinsci.plugins.gitUserContent;
 
 import hudson.Extension;
-import hudson.model.RootAction;
+import hudson.model.UnprotectedRootAction;
+import hudson.security.ACL;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
 import jenkins.model.Jenkins;
 import org.jenkinsci.main.modules.sshd.SSHD;
 import org.jenkinsci.plugins.gitserver.FileBackedHttpGitRepository;
 
 import javax.inject.Inject;
 import java.io.File;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 
 /**
  * Exposes Git repository at http://server/jenkins/userContent.git
@@ -15,7 +21,7 @@ import java.io.File;
  * @author Kohsuke Kawaguchi
  */
 @Extension
-public class GitUserContentRepository extends FileBackedHttpGitRepository implements RootAction {
+public class GitUserContentRepository extends FileBackedHttpGitRepository implements UnprotectedRootAction {
     @Inject
     public SSHD sshd;
 
@@ -38,5 +44,16 @@ public class GitUserContentRepository extends FileBackedHttpGitRepository implem
 
     public String getUrlName() {
         return "userContent.git";
+    }
+
+    @Override
+    public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+        if (!Jenkins.get().hasPermission(Jenkins.READ) && ACL.isAnonymous2(Jenkins.getAuthentication2())) {
+            rsp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            rsp.setHeader("WWW-Authenticate", "Basic realm=\"Jenkins user\"");
+            return;
+        }
+        Jenkins.get().checkPermission(Jenkins.READ);
+        super.doDynamic(req, rsp);
     }
 }
